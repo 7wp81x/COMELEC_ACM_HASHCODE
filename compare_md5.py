46afbd16d5d6760c858068ca8871e87087154e80
7wp81x <justergaming06@gmail.com>
2025-05-13 18:15:37 +0800

    Update files

--- compare_md5.py
+++ compare_md5.py
@@ -20,6 +20,7 @@ def compare_csv(old_csv, new_csv):
 
 	modified_files = []
 	new_files = []
+	not_modified_files = []
 
 	for filename, new_hash in new_dict.items():
 		old_hash = old_dict.get(filename)
@@ -27,8 +28,10 @@ def compare_csv(old_csv, new_csv):
 			new_files.append((filename, new_hash))
 		elif old_hash != new_hash:
 			modified_files.append((filename, old_hash, new_hash))
+		else:
+			not_modified_files.append((filename, old_hash, new_hash))
 
-	return modified_files, new_files
+	return modified_files, new_files, not_modified_files
 
 def display_results(modified, new):
 	print("\033[1;96m==== All Modified Files =====\033[0m")
@@ -45,23 +48,29 @@ def display_results(modified, new):
 			print(f"\033[1;91m{filename}\n  \033[0mOld Hash: \033[1;93m{old_hash}\n \033[0m New Hash: \033[1;92m{new_hash}\n")
 
 
-	with open("results_all_modified_files.txt", "a+") as nf:
+	with open("results/results_all_modified_files.txt", "w+") as nf:
+		nf.write("old_hash|new_hash|filename\n")
 		for filename, old_hash, new_hash in modified:
 			nf.write(f"{old_hash}|{new_hash}|{filename}\n")
+		nf.close()
 
 	print("[+] All Modified files saved to: results_all_modified_files.txt")
 
-	with open("results_all_new_files.txt", "a+") as nf:
+	with open("results/results_all_new_files.txt", "w+") as nf:
+		nf.write("new_hash|filename\n")
 		for filename, new_hash in new:
 			nf.write(f"{new_hash}|{filename}\n")
+		nf.close()
 
 	print("[+] All new files saved to: results_all_new_files.txt")
 
-	with open("results_modified_src_files.txt", "a+") as nf:
+	with open("results/results_modified_src_files.txt", "w+") as nf:
+		nf.write("old_hash|new_hash|filename\n")
 		for filename, old_hash, new_hash in modified:
-			nf.write(f"{old_hash}|{new_hash}|{filename}\n")
+			if "build" not in str(filename).lower():
+				nf.write(f"{old_hash}|{new_hash}|{filename}\n")
 		print("[+] Modified source codes saved to: results_modified_src_files.txt")
-
+		nf.close()
 
 
 if __name__ == "__main__":
@@ -72,5 +81,13 @@ if __name__ == "__main__":
 	old_csv = sys.argv[1]
 	new_csv = sys.argv[2]
 
-	modified, new = compare_csv(old_csv, new_csv)
+	modified, new, not_modified_files = compare_csv(old_csv, new_csv)
 	display_results(modified, new)
+
+	with open("results/results_not_modified_files.txt", "w+") as nf:
+		nf.write("old_hash|new_hash|filename\n")
+		for filename, old_hash, new_hash in not_modified_files:
+			if "build" not in str(filename).lower():
+				nf.write(f"{old_hash}|{new_hash}|{filename}\n")
+		print("[+] Not Modified source codes saved to: results_not_modified_src_files.txt")
+		nf.close()
